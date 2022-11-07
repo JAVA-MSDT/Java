47425d7dedf8eca38e3180932cfc50c1ca54d6d1
Ahmed Samy <serenitydiver@hotmail.com>
2022-11-07 15:13:20 +0100

    Adding more examples of the utils

---
+++ utilshint/src/main/java/org/epamedu/UUIDUtil.java
@@ -0,0 +1,12 @@
+package org.epamedu;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+
+    public static void main(String[] args) {
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid);
+    }
+
+}
