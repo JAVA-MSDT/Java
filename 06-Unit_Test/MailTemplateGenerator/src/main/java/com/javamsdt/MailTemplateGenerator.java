609c82d96d54abddb23ad52a0bde9c0898b18850
Ahmed Samy <serenitydiver@hotmail.com>
2023-12-29 15:11:15 +0100

    Initializng the email generator project

---
+++ 06-Unit_Test/MailTemplateGenerator/src/main/java/com/javamsdt/MailTemplateGenerator.java
@@ -0,0 +1,28 @@
+package com.javamsdt;
+
+import java.util.Map;
+
+public class MailTemplateGenerator {
+
+    private final String template;
+
+    public MailTemplateGenerator(String template) {
+        this.template = template;
+    }
+
+    public String generateTemplate(Map<String, String> values) {
+        String result = template;
+        for (Map.Entry<String, String> entry : values.entrySet()) {
+            String placeholder = "#{" + entry.getKey() + "}";
+            if(template.contains(placeholder)) {
+                result = result.replace(placeholder, entry.getValue());
+            } else {
+                return template;
+            }
+        }
+        if (result.contains("#{")) {
+            throw new RuntimeException("Not all placeholder values provided");
+        }
+        return result;
+    }
+}
